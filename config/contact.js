5c5741e9105d19aa8af9df9b23d6b697cdf5109d
ram-labs1 <ramshyam18656@gmail.com>
2026-04-12 18:22:53 +0530

    Create contact.js

---
+++ config/contact.js
@@ -0,0 +1,8 @@
+export const OWNER_NAME = "Manish Sharma";
+
+export const PHONE_NUMBER = "919625782252";
+
+export const WHATSAPP_LINK =
+  "https://wa.me/919625782252?text= Hello KD Aqua mujhe water supply ki need hai;
+
+export const CALL_LINK = "tel:919625782252";
