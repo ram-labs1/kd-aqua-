b9c87986b637b11bed9ce16a79a048a236a21a31
Ram shyam <ramshyam18656@gmail.com>
2026-04-13 17:39:26 +0530

    moved contact details to config file

--- config/contact.js
+++ config/contact.js
@@ -1,8 +1,5 @@
-export const OWNER_NAME = "Manish Sharma";
-
-export const PHONE_NUMBER = "919625782252";
-
-export const WHATSAPP_LINK =
-  "https://wa.me/919625782252?text= Hello KD Aqua mujhe water supply ki need hai;
-
-export const CALL_LINK = "tel:919625782252";
+export const CONTACT = {
+  phone: "919625782252",
+  owner: "Manish Sharma",
+  whatsappMessage: "Hello Manish i want to order water."
+};
